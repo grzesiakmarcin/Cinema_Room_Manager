5b9a56c75d15fed47fb47db4376adc2832627f08
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-10 13:48:54 +0100

    welcome to the next stage

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -3,6 +3,43 @@ package cinema;
 public class Cinema {
 
     public static void main(String[] args) {
-        // Write your code here
+
+        Bebechy playMe = new Bebechy();
+
+        playMe.printCinema(playMe.cinema);
+
+
+
+
+
+
+
+    }
+}
+
+class Bebechy{
+
+    String[][] cinema={
+            { " ","1","2","3","4","5","6","7","8"},
+            {"1", "S", "S", "S", "S", "S", "S", "S","S"},
+            {"2", "S", "S", "S", "S", "S", "S", "S","S"},
+            {"3", "S", "S", "S", "S", "S", "S", "S","S"},
+            {"4", "S", "S", "S", "S", "S", "S", "S","S"},
+            {"5", "S", "S", "S", "S", "S", "S", "S","S"},
+            {"6", "S", "S", "S", "S", "S", "S", "S","S"},
+            {"7", "S", "S", "S", "S", "S", "S", "S","S"}};
+
+
+    public void printCinema(String[][] array){
+        System.out.println("Cinema:");
+
+
+        for (int i=0; i< array.length;i++){
+            for (int j=0; j<array[i].length;j++ ){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
